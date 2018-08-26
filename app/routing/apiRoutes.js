60260104a2a553706c3c0349ff052b5b71316e41
julieores <you@example.com>
2018-08-25 23:09:45 -0500

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,32 +1,40 @@
 var friendsArray = require("../data/friends.js");
 
 
-module.exports = function(app){
+module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
         res.sendFile(path.join(__dirname, "../api/friends.html"));
     });
 
-    app.post("/api/friends", function(req, res){
+    app.post("/api/friends", function (req, res) {
         console.log(req.body);
+    });
 
-        var newFriend = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: req.body.scores.map(function(score){
-                return parseInt(score);
-            })
-        }
+    var newFriend = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: req.body.scores.map(function (score) {
+            return parseInt(score);
 
-        // loop through friendsArray 
-                // inner loop (inner for loop) of each friends score relative to the new friends score for each question
-                // determine absolute value difference of each score
 
-        
-        
+            // loop through friendsArray 
+
+            for (var i = 0; i < (friendsArray.length - 1); i++) {
 
                 // consolidate each array of scores to a single score or value (look into array.reduce)
                 // figure out what index in our friends array belongs to the lowest single value score
-    })
+                for (var j = 0; j < 10; j++) {
+                    totalDifference += Math.abs(friendsArray[i].scores[j] - newFriend.scores[j]);
+                }
+                allDifferences.push(totalDifference);
+                totalDifference = 0;
+            }
 
+            var bestMatch = friendsArray[allDifferences.indexOf(Math.min.apply(null, allDifferences))];
+
+            res.send(bestMatch);
+            console.log(bestMatch);
+        })
+    }
 }
--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var bodyParser = require("body-parser")
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
-
-
-app.listen(PORT, function(){
-console.log("App listening on PORT " + PORT);
-
-});
+app.use(bodyParser.text());
 
 require('./app/routing/htmlRoutes.js')(app);
 require('.app/routing/apiRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+
+});
